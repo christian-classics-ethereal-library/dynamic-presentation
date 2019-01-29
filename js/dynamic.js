8a6fc87528b34e586c7718a49554eeb8a91c7c01
Zach DeCook <zdecook@ccel.org>
2019-01-29 14:28:28 -0500

    * Dynamic Presentation: Change Y Position with javascript

--- js/dynamic.js
+++ js/dynamic.js
@@ -4,6 +4,7 @@ $(document).ready(function(){
     switchVerse('v1', 'data-v2')
     switchVerse('v2', 'data-v3')
     switchVerse('v3', 'data-v4')
+    textYPosition('data-y-bottom');
 });
 
 /**
@@ -12,8 +13,16 @@ $(document).ready(function(){
  * @param string verseAttr The attribute containing the text that you want to switch to.
  */
 function switchVerse(id, verseAttr) {
+    // TODO: Remove the text even when the verse we want isn't present.
     var els = $('#' + id + ' svg g text[' + verseAttr + ']');
     for (var i = 0; i < els.length; i++) {
         els[i].innerHTML = $(els[i]).attr(verseAttr);
+    }
+}
+
+function textYPosition(yPositionAttr) {
+    var els = $('svg g text[' + yPositionAttr + ']');
+    for (var i = 0; i < els.length; i++) {
+        $(els[i]).attr('y', $(els[i]).attr(yPositionAttr));
     }
 }
