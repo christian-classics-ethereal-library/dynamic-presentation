063888c3f1f48c85d8215f2506abd076998766a3
Zach DeCook <zdecook@ccel.org>
2019-05-08 15:44:10 -0400

    * Dynamic Presentation: Allow toolkit to be switched out

--- js/LoadRevealMusicXMLPlugin.js
+++ js/LoadRevealMusicXMLPlugin.js
@@ -1,11 +1,22 @@
 /* globals Reveal, verovio */
 import { RevealMusicXML } from './RevealMusicXML.js';
-// import { PianoRollToolkit } from './PianoRollToolkit.js';
+import { PianoRollToolkit } from './PianoRollToolkit.js';
 import { MusicXMLTransformer } from './MusicXMLTransformer.js';
 
+function urlParam (name) {
+  var results = new RegExp('[?&]' + name + '=([^&#]*)').exec(
+    window.location.search
+  );
+  return results !== null ? results[1] || 0 : false;
+}
+
 let mxt = new MusicXMLTransformer();
-// eslint-disable-next-line
-let tk = new verovio.toolkit();
-// let tk = new PianoRollToolkit();
+let tk;
+if (urlParam('toolkit') === 'verovio') {
+  // eslint-disable-next-line
+  tk = new verovio.toolkit();
+} else {
+  tk = new PianoRollToolkit();
+}
 let rmx = new RevealMusicXML(tk, mxt);
 Reveal.registerPlugin('musicxml', rmx);
