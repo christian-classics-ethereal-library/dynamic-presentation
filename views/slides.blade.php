5358b5c9799646603e260d5828aabb99364c3001
Zach DeCook <zdecook@ccel.org>
2019-05-20 13:31:53 -0400

    * Dynamic Presentation: Use non-minified javascript for debug mode

--- views/slides.blade.php
+++ views/slides.blade.php
@@ -11,8 +11,8 @@
     <script src="{{ asset('js/app.js') }}" defer></script>
     <script src="{{ asset('js/slides.js') }}" defer></script>
     @if (isset($scripts))
-        @foreach ($scripts as $script)
-            <script src="{{ $script }}" defer></script>
+        @foreach ($scripts as $script => $type)
+            <script type='{{ $type }}'src="{{ $script }}" defer></script>
         @endforeach
     @endif
     <link href="{{ asset('css/reveal.css') }}" rel="stylesheet">
