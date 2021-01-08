1960c884c85ecb3a8b5d4b273ac0901670af7837
Zach DeCook <zdecook@ccel.org>
2021-01-08 16:16:46 -0500

    * Dynamic Presentation: Bump version

--- js/LoadRevealMusicXMLPlugin.js
+++ js/LoadRevealMusicXMLPlugin.js
@@ -1,16 +1,16 @@
 /* globals Reveal */
-import { AudioPlayer } from '../js/AudioPlayer.js';
-import { MIDIPlayer } from '../js/MIDIPlayer.js';
+import { AudioPlayer } from '../js/AudioPlayer.js?v=1.6.0';
+import { MIDIPlayer } from '../js/MIDIPlayer.js?v=1.6.0';
 import { MusicToolkit } from '../js/MusicToolkit.js?v=1.3.2';
 import { MusicXMLTransformer } from '../js/MusicXMLTransformer.js';
 import { PianoRollToolkit } from '../js/PianoRollToolkit.js?v=1.3.3';
 import { RevealMusicArranger } from '../js/RevealMusicArranger.js';
-import { RevealMusicXML } from '../js/RevealMusicXML.js?v=1.4.0';
+import { RevealMusicXML } from '../js/RevealMusicXML.js?v=1.6.0';
 import { TextOnlyToolkit } from '../js/TextOnlyToolkit.js?v=1.3.3'; // Depends on PianoRollToolkit
 import { urlParam } from '../js/urlParam.js';
-import { XMLPlayer } from '../js/XMLPlayer.js'; // Depends on MIDIPlayer
-import { YouTubePlayer } from '../js/YouTubePlayer.js';
-import { VoidPlayer } from '../js/VoidPlayer.js';
+import { XMLPlayer } from '../js/XMLPlayer.js?v=1.6.0'; // Depends on MIDIPlayer
+import { YouTubePlayer } from '../js/YouTubePlayer.js?v=1.6.0';
+import { VoidPlayer } from '../js/VoidPlayer.js?v=1.6.0';
 
 let rma = new RevealMusicArranger();
 
